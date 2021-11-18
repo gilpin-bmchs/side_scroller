fb9c2e9493bb12bfe310a1bc0de37c716f2cfcd8
David Gilpin <dgilpin@bmchs.org>
2021-11-18 15:01:51 -0600

    added ship and up and down movement

--- pygame_sidescroller.py
+++ pygame_sidescroller.py
@@ -4,8 +4,13 @@ from pygame.locals import *
 
 pygame.init()
 
+# FPS Clock
+clock = pygame.time.Clock()
+FPS = 60
+
 # colors
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 # variables
 SCREEN_WIDTH = 800
@@ -22,11 +27,23 @@ class Ship(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.image = pygame.image.load("ship.png")
+        self.image = pygame.transform.rotate(pygame.image.load("ship.png"), -90)
         self.rect = self.image.get_rect()
-        self.rect.center = (0, 200)
+
+        self.rect.center = (20, 200)
+
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+
+        if self.rect.top > 0:
+            if pressed_keys[K_DOWN]:
+                self.rect.move_ip(0, 5)
+        if self.rect.bottom < SCREEN_HEIGHT:
+            if pressed_keys[K_UP]:
+                self.rect.move_ip(0, -5)
 
 SHIP = Ship()
+# pygame.transform.rotate(SHIP.image, 90)
 # Loop
 while True:
     for event in pygame.event.get():
@@ -34,6 +51,9 @@ while True:
             pygame.quit()
             sys.exit()
 
+    SHIP.move()
+    # pygame.draw.rect(DISPLAYSURF, RED, SHIP.rect)
     DISPLAYSURF.blit(SHIP.image, SHIP.rect)
 
     pygame.display.update()
+    clock.tick(FPS)
