e72cf50032fbaaeb6798e0a7f962c36d00e89875
David Gilpin <dgilpin@bmchs.org>
2021-11-19 10:42:59 -0600

    added asteroid that moves from right to left repeatedly

--- pygame_sidescroller.py
+++ pygame_sidescroller.py
@@ -20,7 +20,7 @@ BACKGROUND = pygame.image.load("background.png")
 
 # setup display
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-DISPLAYSURF.fill(WHITE)
+# DISPLAYSURF.fill(WHITE)
 DISPLAYSURF.blit(BACKGROUND, (0,0))
 
 class Ship(pygame.sprite.Sprite):
@@ -35,15 +35,38 @@ class Ship(pygame.sprite.Sprite):
     def move(self):
         pressed_keys = pygame.key.get_pressed()
 
-        if self.rect.top > 0:
+        if self.rect.bottom < SCREEN_HEIGHT:
             if pressed_keys[K_DOWN]:
                 self.rect.move_ip(0, 5)
-        if self.rect.bottom < SCREEN_HEIGHT:
+        if self.rect.top > 0:
             if pressed_keys[K_UP]:
                 self.rect.move_ip(0, -5)
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.image.load("Asteroid2.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (SCREEN_WIDTH + 0.5 * self.rect.width, 100)
+
+    def move(self):
+        self.rect.move_ip(-5, 0)
+        if self.rect.right < 0:
+            self.rect.left = SCREEN_WIDTH
+
+# create player and enemy objects
 SHIP = Ship()
-# pygame.transform.rotate(SHIP.image, 90)
+ASTEROID = Enemy()
+
+# create sprite groups
+enemies = pygame.sprite.Group()
+enemies.add(ASTEROID)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(SHIP)
+all_sprites.add(ASTEROID)
+
+
 # Loop
 while True:
     for event in pygame.event.get():
@@ -51,9 +74,16 @@ while True:
             pygame.quit()
             sys.exit()
 
-    SHIP.move()
     # pygame.draw.rect(DISPLAYSURF, RED, SHIP.rect)
-    DISPLAYSURF.blit(SHIP.image, SHIP.rect)
+    DISPLAYSURF.blit(BACKGROUND, (0,0))
+
+    # moves and redraws all sprites
+    for entity in all_sprites:
+        entity.move()
+        DISPLAYSURF.blit(entity.image, entity.rect)
+
+    # DISPLAYSURF.blit(SHIP.image, SHIP.rect)
+    # DISPLAYSURF.blit(ASTEROID.image, (200, 200))
 
     pygame.display.update()
     clock.tick(FPS)
